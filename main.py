54b883b99e383f721e044b11fa218923da7c8626
JDoosk <minimus713@gmail.com>
2025-07-21 18:48:55 -0700

    refactor: cleaning up main to be more abstract and splitting out vanilla scan logic

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from vanilla_scan import vanilla_scan
+import socket
 
 #0-1023 are well-known and standardized ports assigned by the IANA (Internet Assigned Numbers Authority)
 
 def main():
-    startPort = 1
-    finalPort = 2#65535
-    for portNum in range(startPort, finalPort+1):
-        print('Pinging port number', portNum)
-        beeep = vanilla_scan(portNum)
-        print(beeep)
+    host_ip = socket.gethostbyname('www.google.com')
+    print(host_ip)
+
+    print('Running vanilla scan: ')
+    vanilla_scan(host_ip)
 
 main()
--- vanilla_scan.py
+++ vanilla_scan.py
@@ -1,15 +1,22 @@
 import socket
 import sys
+socket.setdefaulttimeout(1) #Set timeout to 1 sec for all sockets
 
-def vanilla_scan(port_num):
+def vanilla_scan(host_ip):
+    startPort = 1
+    finalPort = 2#65535
+
+
+    for portNum in range(startPort, finalPort+1):
+        print('Pinging port number', portNum)
+        single_port_scan(host_ip, portNum)
+
+def single_port_scan(host_ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.settimeout(1)
-    print("Socket created successfully")        
+
     try:
-        host_ip = socket.gethostbyname('www.google.com')
-        print(host_ip)
-        conn = s.connect((host_ip, port_num))
+        conn = s.connect((host_ip, port))
         print(conn)
         print(s.recv(1024))
         conn.close()
@@ -18,8 +25,8 @@ def vanilla_scan(port_num):
     except KeyboardInterrupt:
         print("Exiting Program.")
         sys.exit()
+    except ConnectionRefusedError:
+        print('closed')
     except Exception as e:
+        print(f"error: {e}")
         pass
-
-
-    return port_num
